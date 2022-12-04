fc25ac9de763968547d1fd596866c14d65c1e30b
Mohammad Ahmadi <iqbal_dyncorp@live.com>
2022-12-04 11:33:52 -0800

    installed npm i, npm install --save-dev jest, npm i jest, and created test folder that includes test files for each individual person/position

---
+++ __tests__/managerTest.js
@@ -0,0 +1,21 @@
+const manager = require('../lib/manager')
+const employee = require('../lib/employee')
+
+test('Can set office number via constructor argument', () => {
+  const testValue = 100
+  const e = new manager('Iqbal', 1, 'test@test.com', testValue)
+  // testing a value along with the matcher function toBe(testValue)
+  expect(e.officeNumber).toBe(testValue)
+})
+
+test('getRole() should return "manager"', () => {
+  const testValue = 'manager'
+  const e = new manager('Iqbal', 1, 100)
+  expect(e.getRole()).toBe(testValue)
+})
+
+test('Can get office number via getOffice()', () => {
+  const testValue = 100
+  const e = new manager('Iqbal', 1, 'test@test.com', testValue)
+  expect(e.getOfficNumber()).toBe(testValue)
+})
--- source/landing-page-template.js
+++ source/landing-page-template.js
@@ -56,6 +56,28 @@ const generateWholeTeam = (team) => {
 </div>
     `
   }
+
+  const html = []
+
+  html.push(
+    team
+      .filter((employee) => employee.getRole() === 'Manager')
+      .map((manager) => generateManager(manager))
+  )
+  html.push(
+    team
+      .filter((employee) => employee.getRole() === 'Engineer')
+      .map((engineer) => generateEngineer(engineer))
+      .join('')
+  )
+  html.push(
+    team
+      .filter((employee) => employee.getRole() === 'Intern')
+      .map((intern) => generateIntern(intern))
+      .join('')
+  )
+
+  return html.join('')
 }
 // generate html page
 module.export = (team) => {
